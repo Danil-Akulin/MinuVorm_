8e2b16d4847da7bbbdb7add94b323958642c6b13
Opilane TTHK <opilane@tthk.ee>
2022-02-11 15:29:21 +0200

    Add project files.

---
+++ admin_panel.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MinuVorm
+{
+    class Admin_Form : System.Windows.Forms.Form
+    {
+        static string conn_KinoDB = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\opilane\source\repos\Danil_Akulin_TARpv20\MinuVorm\appData\Kino_DB.mdf;Integrated Security=True;Connect Timeout=30";
+        SqlConnection connect_to_DB = new SqlConnection(conn_KinoDB);
+
+        SqlCommand command;
+
+        SqlDataAdapter adapter;
+        Button film_uuenda, film_kustuta, film_naita;
+        public Admin_Form()
+        {
+
+            this.Size = new System.Drawing.Size(800, 450);
+            this.Text = "Admin Panel";
+            BackColor = Color.White;
+            /*Button pilet_naita = new Button
+            {
+                Location = new System.Drawing.Point(50, 50),
+                Size = new System.Drawing.Size(80, 25),
+                Text = "Ostetud \npiletid"
+            };
+            this.Controls.Add(pilet_naita);
+            pilet_naita.Click += Pilet_naita_Click;*/
+            film_naita = new Button
+            {
+                Location = new System.Drawing.Point(10, 10),
+                Size = new System.Drawing.Size(120, 40),
+                Font = new Font("Times New Roman", 15.0f),
+                Text = "Näita filmid",
+
+            };
+
+            this.Controls.Add(film_naita);
+            film_naita.Click += Film_naita_Click;
+            /*film_naita.Click += Film_naita_MouseWheel;*/
+            film_uuenda = new Button
+            {
+                Location = new System.Drawing.Point(650, 70),
+                Size = new System.Drawing.Size(120, 40),
+                Font = new Font("Times New Roman", 15.0f),
+                Text = "Uuendamine",
+
+        };
+            this.Controls.Add(film_uuenda);
+            film_uuenda.Click += Film_uuenda_Click;
+            film_kustuta = new Button
+            {
+                Location = new System.Drawing.Point(650, 10),
+                Size = new System.Drawing.Size(120, 40),
+                Font = new Font("Times New Roman", 15.0f),
+                Text = "Kustutamine",
+
+
+            };
+            this.Controls.Add(film_kustuta);
+        }
+
+        static int Id = 0;
+
+        private void Film_uuenda_Click(object sender, EventArgs e)
+        {
+
+            if (film_txt.Text != "" && aasta_txt.Text != "" && poster_txt.Text != "" && poster.Image != null)
+            {
+                connect_to_DB.Open();
+                command = new SqlCommand("UPDATE Filmid  SET Filmi_nimetus=@film,Aasta=@aasta,Poster=@poster WHERE Id=@id", connect_to_DB);
+
+                command.Parameters.AddWithValue("@id", Id);
+                command.Parameters.AddWithValue("@film", film_txt.Text);
+                command.Parameters.AddWithValue("@aasta", aasta_txt.Text);
+                command.Parameters.AddWithValue("@poster", poster_txt.Text);
+                //string file_pilt = poster_txt.Text + ".jpg";
+                //command.Parameters.AddWithValue("@poster", file_pilt);
+                command.ExecuteNonQuery();
+                connect_to_DB.Close();
+                ClearData();
+                Data();
+                MessageBox.Show("Andmed uuendatud");
+            }
+            else
+            {
+                MessageBox.Show("Viga");
+            }
+
+        }
+
+        private void Pilet_naita_Click(object sender, EventArgs e)
+        {
+            connect_to_DB.Open();
+            DataTable tabel_p = new DataTable();
+            DataGridView dataGridView_p = new DataGridView();
+            DataSet dataset_p = new DataSet();
+            SqlDataAdapter adapter_p = new SqlDataAdapter("SELECT Rida,Koht,Film_Id FROM [dbo].[Piletid]; SELECT Filmi_nimetus FROM [dbo].[Filmid]", connect_to_DB);
+               MessageBox.Show("HI");
+            //adapter_p.TableMappings.Add("Piletid", "Rida");
+            //adapter_p.TableMappings.Add("Filmid", "Filmi_nimetus");
+            //adapter_p.Fill(dataset_p);
+            adapter_p.Fill(tabel_p);
+            dataGridView_p.DataSource = tabel_p;
+            dataGridView_p.Location = new System.Drawing.Point(10, 75);
+            dataGridView_p.Size = new System.Drawing.Size(400, 200);
+
+
+            SqlDataAdapter adapter_f = new SqlDataAdapter("SELECT Filmi_nimetus FROM [dbo].[Filmid]", connect_to_DB);
+            DataTable tabel_f = new DataTable();
+            DataSet dataset_f = new DataSet();
+            adapter_f.Fill(tabel_f);
+            /*fkc = new ForeignKeyConstraint(tabel_f.Columns["Id"], tabel_p.Columns["Film_Id"]);
+            tabel_p.Constraints.Add(fkc);*/
+            poster.Image = Image.FromFile("../../Posterid/Start.jpg");
+
+            DataGridViewComboBoxCell cbc = new DataGridViewComboBoxCell();
+            ComboBox com_f = new ComboBox();
+            foreach (DataRow row in tabel_f.Rows)
+            {
+                com_f.Items.Add(row["Filmi_nimetus"]);
+                cbc.Items.Add(row["Filmi_nimetus"]);
+            }
+            cbc.Value = com_f;
+            connect_to_DB.Close();
+            this.Controls.Add(dataGridView_p);
+            this.Controls.Add(com_f);
+        }
+
+
+        TextBox film_txt, aasta_txt, poster_txt;
+        PictureBox poster;
+        DataGridView dataGridView;
+        DataTable tabel;
+        private void Film_naita_Click(object sender, EventArgs e)
+        {
+            film_naita.Text = "Peida filmid";
+            film_uuenda.Visible = true;
+            film_kustuta.Visible = true;
+
+            film_txt = new TextBox
+            { Location = new System.Drawing.Point(450, 75) };
+            aasta_txt = new TextBox
+            { Location = new System.Drawing.Point(450, 100) };
+            poster_txt = new TextBox
+            { Location = new System.Drawing.Point(450, 125) };
+            poster = new PictureBox
+            {
+                Size = new System.Drawing.Size(180, 250),
+                Location = new System.Drawing.Point(450, 150),
+                Image = Image.FromFile("../../Posterid/Start.jpg")
+            };
+
+            Data();
+            /*this.Controls.Add(dataGridView);
+            this.Controls.Add(film_txt);
+            this.Controls.Add(aasta_txt);
+            this.Controls.Add(poster_txt);
+            this.Controls.Add(poster);*/
+
+        }
+            public void Data()
+        {
+            connect_to_DB.Open();
+            tabel = new DataTable();
+            dataGridView = new DataGridView();
+            dataGridView.RowHeaderMouseClick += DataGridView_RowHeaderMouseClick;
+            adapter = new SqlDataAdapter("SELECT * FROM [dbo].[Kino]", connect_to_DB);
+            adapter.Fill(tabel);
+            dataGridView.DataSource = tabel;
+            dataGridView.Location = new System.Drawing.Point(10, 75);
+            dataGridView.Size = new System.Drawing.Size(400, 200);
+            connect_to_DB.Close();
+            this.Controls.Add(dataGridView);
+            this.Controls.Add(film_txt);
+            this.Controls.Add(aasta_txt);
+            this.Controls.Add(poster_txt);
+            this.Controls.Add(poster);
+        }
+        private void DataGridView_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            Id = Convert.ToInt32(dataGridView.Rows[e.RowIndex].Cells[0].Value.ToString());
+            film_txt.Text = dataGridView.Rows[e.RowIndex].Cells[1].Value.ToString();
+            aasta_txt.Text = dataGridView.Rows[e.RowIndex].Cells[2].Value.ToString();
+            poster_txt.Text = dataGridView.Rows[e.RowIndex].Cells[3].Value.ToString();
+            poster.Image = Image.FromFile(@"..\..\Posterid\" + dataGridView.Rows[e.RowIndex].Cells[3].Value.ToString());
+            this.Text = dataGridView.Rows[e.RowIndex].Cells[1].Value.ToString();
+
+        }
+
+        /*private void Film_naita_MouseWheel(object sender, EventArgs e)
+        {
+            film_naita.Text = "123";
+        }*/
+
+        private void ClearData()
+        {
+            //Id = 0;
+            film_txt.Text = "";
+            aasta_txt.Text = "";
+            poster_txt.Text = "";
+            //save.FileName = "";
+            poster.Image = Image.FromFile("../../Posterid/Start.jpg");
+
+        }
+    }
+}
---
+++ login.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MinuVorm
+{
+    public partial class login : Form
+    {
+        public PictureBox img = new PictureBox();
+
+        public TextBox box = new TextBox();
+
+        public TextBox box2 = new TextBox();
+
+        public TextBox box3 = new TextBox();
+
+        public TextBox box4 = new TextBox();
+
+        public TextBox box5 = new TextBox();
+
+        public login()
+        {
+            this.ClientSize = new System.Drawing.Size(900, 500);
+            this.Text = "Login";
+            img.Size = new Size(900, 508);
+            img.Image = Image.FromFile(@"..\..\img\koik.jpg");
+
+
+
+
+
+            box.Location = new Point(300, 200);
+            box.Text = "Login";
+            Font myfont = new Font("Times New Roman", 25.0f);    
+            box.Font = myfont;                                   
+            box.ReadOnly = true;                               
+            box.Height = 250;
+            box.Width = 80;
+            box.BackColor = Color.FromArgb(255, 207, 215, 226);
+            box.BorderStyle = 0;                            
+            this.Controls.Add(box);
+
+
+            box2.Location = new Point(300, 250);
+            box2.Text = "Password";
+            Font myfont2 = new Font("Times New Roman", 25.0f);
+            box2.Font = myfont2;
+            box2.ReadOnly = true;
+            box2.Height = 250;
+            box2.Width = 140;
+            box2.BackColor = Color.FromArgb(255, 136, 145, 174);
+            box2.BorderStyle = 0;
+            this.Controls.Add(box2);
+            this.Controls.Add(img);
+
+
+            box3.Location = new Point(450, 250);
+            box3.Text = "Your password";
+            Font myfont3 = new Font("Times New Roman", 20.0f);
+            box3.Font = myfont3;
+            box3.ReadOnly = false;
+            box3.Height = 250;
+            box3.Width = 160;
+            box3.BackColor = Color.FromArgb(255, 193, 198, 213);
+            box3.BorderStyle = 0;
+            box3.Click += Box3_Click;
+
+
+            box4.Location = new Point(450, 200);
+            box4.Text = "Your login";
+            Font myfont4 = new Font("Times New Roman", 20.0f);
+            box4.Font = myfont4;
+            box4.ReadOnly = false;
+            box4.Height = 250;
+            box4.Width = 160;
+            box4.BackColor = Color.FromArgb(255, 207, 215, 226);
+            box4.BorderStyle = 0;
+            box4.Click += Box4_Click;
+
+            box5.Location = new Point(10, 10);
+            box5.Text = "TEST";
+            Font myfont5 = new Font("Times New Roman", 20.0f);
+            box5.Font = myfont5;
+            box5.ReadOnly = false;
+            box5.Height = 80;
+            box5.Width = 100;
+            box5.BackColor = Color.FromArgb(255, 207, 215, 226);
+            box5.BorderStyle = 0;
+            box5.Click += Box5_Click;
+
+
+
+
+
+            this.Controls.Add(box3);
+            this.Controls.Add(box4);
+            this.Controls.Add(box5);
+            this.Controls.Add(img);
+
+        }
+
+        private void Box4_Click(object sender, EventArgs e)
+        {
+            box4.Clear();
+        }
+
+        private void Box3_Click(object sender, EventArgs e)
+        {
+            box3.Clear();
+        }
+        private void Box5_Click(object sender, EventArgs e)
+        {
+            Admin_Form uus_aken = new Admin_Form();
+            uus_aken.StartPosition = FormStartPosition.CenterScreen;
+            uus_aken.ShowDialog();
+        }
+    }
+
+}
